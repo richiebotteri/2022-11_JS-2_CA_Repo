31fdf0c9a5fd5897d296729da5b93ff18fff11fa
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2022-11-25 10:14:21 +0100

    added changes to send tags with created post

---
+++ js/api/posts/make-post-request.mjs
@@ -0,0 +1,22 @@
+import { SOCIAL_URL } from "../api-environment.mjs";
+import * as localStorage from "../../storage/local-storage.mjs";
+
+export async function makePostRequest(postObject, method, action) {
+   try {
+      console.log("INSIDE API REQUEST", postObject.media);
+      const options = {
+         method: method,
+         headers: {
+            "Content-type": "application/json",
+            Authorization: `Bearer ${localStorage.loadItem("loginToken")}`,
+         },
+         body: JSON.stringify(postObject),
+      };
+      const response = await fetch(`${SOCIAL_URL}${action}`, options);
+      const result = await response.json();
+      console.log("response:", response);
+      console.log("result: ", result);
+   } catch (error) {
+      console.log(error);
+   }
+}
