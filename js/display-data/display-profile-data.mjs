5dce5ad6921f6b22843aaf9db1a5001f676892e5
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2022-12-01 12:30:33 +0100

    added additional function two display profile-data on view-page

---
+++ js/display-data/display-profile-data.mjs
@@ -0,0 +1,30 @@
+import { changeAvatarData } from "../api/change-api-data/change-avatar-data.mjs";
+import { loadItem } from "../storage/local-storage.mjs";
+
+export function displayProfileData() {
+   const profile = loadItem("profile");
+   const profileName = profile.name;
+   const profileAvatar = profile.avatar;
+   const newAvatarElement = changeAvatarData(profileAvatar, profileName, true);
+   const parser = new DOMParser();
+   const parseDocument = parser.parseFromString(
+      `
+      ${newAvatarElement}
+      <h1 class="text-center text-sm-center mt-5 text-secondary fw-semibold">${profileName}</h1>
+      <p id="about-description"class="mt-2">Hi i'm <span class="fw-semibold">${profileName}</span>, I'm a new goose in town looking to find some new friends to float on water with. Looking forward to meeting new, Quack Quack!!</p>
+   `,
+      "text/html"
+   );
+
+   const profileAvatarElement = parseDocument.querySelector("img");
+   const profileNameElement = parseDocument.querySelector("h1");
+   const profileAboutElement = parseDocument.querySelector("#about-description");
+
+   const profileHeaderSection = document.querySelector("#profile-header");
+   const profileAboutSection = document.querySelector("#profile-about");
+
+   console.log(profileAboutElement);
+   profileHeaderSection.appendChild(profileAvatarElement);
+   profileHeaderSection.appendChild(profileNameElement);
+   profileAboutSection.appendChild(profileAboutElement);
+}
--- js/index.mjs
+++ js/index.mjs
@@ -2,6 +2,7 @@ import { validatedForms } from "./form-validation/index.mjs";
 import { offlineFeedback } from "./display-data/display-logout-feedback.mjs";
 import { postHandlers, signupHandlers } from "./handlers/index.mjs";
 import { postRequests } from "./api/posts/index.mjs";
+import { displayProfileData } from "./display-data/display-profile-data.mjs";
 
 const path = location.pathname;
 
@@ -10,6 +11,7 @@ validatedForms();
 if (path === "/profile/view/index.html" || path === "/profile/view/") {
    postHandlers();
    postRequests();
+   displayProfileData();
 } else if (path === "/profile/home-feed/index.html" || path === "/profile/home-feed/") {
    postHandlers();
    postRequests();
