e01980b64f714cceb0734e63abd65bc9a885bedf
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2022-12-08 14:50:03 +0100

    added JsDocs to storage folder files

--- js/storage/delete-stored-profile.mjs
+++ js/storage/delete-stored-profile.mjs
@@ -1,6 +1,24 @@
 import * as storage from "./local-storage.mjs";
 import * as sessionStorage from "./session-storage.mjs";
 
+/**
+ * Remove profile-object from sessionStorage
+ * @example
+ *```js
+ * // deletes Token used to grant
+ * // user access to there profile
+ * storage.deleteItem("loginToken");
+ *
+ * // deletes profile with username,
+ * // email, avatar & banner
+ * storage.deleteItem("profile");
+ *
+ * //Saves a boolean value of true to
+ * //the sessionsStorage to indicate
+ * //that account is not online
+ * sessionStorage.saveSessionItem("online", false);
+ *```
+ */
 export function removeProfileFromStorage() {
    storage.deleteItem("loginToken");
    storage.deleteItem("profile");
