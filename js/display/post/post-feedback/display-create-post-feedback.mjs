430de96da4ed0b96c5f02d10ceec15bc7b2f9055
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2022-12-13 12:18:55 +0100

    adding parserFunction

--- js/display/post/post-feedback/display-create-post-feedback.mjs
+++ js/display/post/post-feedback/display-create-post-feedback.mjs
@@ -1,18 +1,16 @@
 export function displayCreatePostFeedback(responseOk) {
-   const createPostFeedback = document.querySelector(".create-post-feedback");
-   const parser = new DOMParser();
-   const parseDocument = parser.parseFromString(
-      `
-      <div id="create-success">
+   const feedbackContainer = document.querySelector(".create-post-feedback");
+   const feedbackContent = `
+   <div id="create-success">
          <h4 class="card-title text-center fw-semibold">Your post has been created</h4>
          <p class="card-body text-center mb-0">Redirecting to new post</p>
       </div>
-  `,
-      "text/html"
-   );
+   `;
+
+   const parseDocument = createParseDoc(feedbackContent);
 
    if (responseOk) {
-      createPostFeedback.classList.remove("d-none");
-      createPostFeedback.appendChild(parseDocument.getElementById("create-success"));
+      feedbackContainer.classList.remove("d-none");
+      feedbackContainer.appendChild(parseDocument.getElementById("create-success"));
    }
 }
